4eba052b16020ca2e26af6521a47bfcfb59f9ab9
dabay <dabay@labbox.local.asdf>
2017-10-05 12:18:51 -0400

    added user input

--- dealership.py
+++ dealership.py
@@ -21,8 +21,26 @@ class Motorcycle(Vehicle):
 	def __init__(self, make, model, year, hasSideCar):
 		super(Motorcycle, self).__init__(make, model, year)
 		self.hasSideCar = hasSideCar
-		
+'''		
 v = Car('Honda', 'Accord', 2014, 5)
 print v	
-	
-	
+'''
+
+print '\nEnter type of vehicle:', 
+vehicle = raw_input('--> ')
+print '\nEnter make of %s?' % vehicle,
+make = raw_input()
+print '\nEnter model of %s?' % make,
+model = raw_input()
+print '\nEnter year of %s?' % model,
+year = raw_input()
+if '%s' % vehicle is 'car':
+	print '\nEnter number of doors for %s?' % model,
+	numdoor = int(raw_input())
+elif '%s' % vehicle is 'truck':
+	print '\n%s with bed?' % vehicle,
+elif '%s' % vehicle is 'motorcycle':
+	print '\n%s with side cars?' % vehicle,
+print '\nHere are the list of %s %s %s in stock.\n' %(
+year, make, model)
+
